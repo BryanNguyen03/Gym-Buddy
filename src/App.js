50c773e5f37a06aef6874291a96ef5e29ebb2395
Bryan <bryannguyen051012@gmail.com>
2023-01-25 16:47:12 -0500

    Basic Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
+/**
+ * A function that takes in a city name and returns the weather forecast for that city
+ * @param {*} city Name of the city
+ */
+const getWeather = (city) => {};
+
 function App() {
+  const [userLocation, setUserLocation] = useState("Toronto");
+
   return (
     <div className="Weather">
+      <h1>Location: {userLocation}</h1>
       <h1>Weather</h1>
     </div>
   );
